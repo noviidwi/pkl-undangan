306382a1602bc447b59d0191d7ea1509a4d3f2c2
pkl-undangan <novidwifitriani@students.undip.ac.id>
2024-02-05 13:16:39 +0700

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <div id="beranda" class="mainPage">
         <div class="topbar" >
             <div class="wrapperTopbar">
-                <img src="{{asset('home/logo.svg')}}">
+                <img src="{{asset('favicon/logo.png')}}">
                 <div class="wrapperList">
                     <a class="redirect" href="#model">Model</a>
                     <a class="redirect" href="#method">Cara Pembelian</a>
@@ -36,41 +36,8 @@
         </div>
         <img src="{{ asset('home/homeMockup.png') }}"class="mockupAnimation">
     </div>
-    <div class="feature">
-        <div class="gridWrapper">
-            <div class="card" data-aos="fade-down" data-aos-once="true">
-                <img src="{{ asset('home/lowPrice.png') }}">
-                <div class="title">Harga Terjangkau</div>
-                <div class="subTitle">Dimulai dari 30 ribu, anda sudah mendapatkan undangan digital dari i-wedding</div>
-            </div>
-            <div class="card" data-aos="fade-down" data-aos-once="true">
-                <img src="{{ asset('home/lifetime.png') }}">
-                <div class="title">Link Aktif Selamanya</div>
-                <div class="subTitle">Undangan tetap tersedia dan dapat diakses kapan saja, di mana saja, seumur hidup</div>
-            </div>
-            <div class="card" data-aos="fade-down" data-aos-once="true">
-                <img src="{{ asset('home/easy.png') }}">
-                <div class="title">Kemudahan Pembaruan</div>
-                <div class="subTitle">Perubahan jadwal, lokasi, atau informasi lainnya, dapat dirubah dengan <a
-                    href="https://wa.me/6289657670704" target="_blank">mengirim pesan</a></div>
-            </div>
-        </div>
     </div>
     <div class="midPage">
-        <div class="content">
-            Beberapa pasangan mempercayai untuk membuat undangan digital pernikahan mereka di
-            <div>diundang.shop</div>
-        </div>
-        <div class="cardWrapper">
-            <div class="card" data-aos="fade-right" data-aos-once="true">
-                <div class="number">{{ $posts }}</div>
-                <div class="title">Undangan dibuat</div>
-            </div>
-            <div class="card" data-aos="fade-left" data-aos-once="true">
-                <div class="number">{{ $comments }}</div>
-                <div class="title">Ucapan dan do'a</div>
-            </div>
-        </div>
         <div class="model" id="model">
             <div class="header">
                 Model
@@ -102,42 +69,6 @@
                 <div class="price">Rp. 40.000</div>
                 <div class="title">Javva-Navue</div>
                 <a href="/budi-maya" target="_blank"><img src="{{ asset('home/eye.svg') }}"> lihat demo</a>
-            </div>
-            <div class="card" data-aos="zoom-in" data-aos-once="true">
-                <img src="{{ asset('Mockup/jg.png') }}">
-                <div class="price">Rp. 40.000</div>
-                <div class="title">Javva-Gremy</div>
-                <a href="/adnan-anisa" target="_blank"><img src="{{ asset('home/eye.svg') }}"> lihat demo</a>
-            </div>
-            <div class="card" data-aos="zoom-in" data-aos-once="true">
-                <img src="{{ asset('Mockup/jb.png') }}">
-                <div class="price">Rp. 40.000</div>
-                <div class="title">Javva-Bream</div>
-                <a href="/arif-dewi" target="_blank"><img src="{{ asset('home/eye.svg') }}"> lihat demo</a>
-            </div>
-            <div class="card" data-aos="zoom-in" data-aos-once="true">
-                <img src="{{ asset('Mockup/ln.png') }}">
-                <div class="price">Rp. 45.000</div>
-                <div class="title">Luxus-Navue</div>
-                <a href="/bima-dina" target="_blank"><img src="{{ asset('home/eye.svg') }}"> lihat demo</a>
-            </div>
-            <div class="card" data-aos="zoom-in" data-aos-once="true">
-                <img src="{{ asset('Mockup/lb.png') }}">
-                <div class="price">Rp. 45.000</div>
-                <div class="title">Luxus-Bream</div>
-                <a href="/fadli-nia" target="_blank"><img src="{{ asset('home/eye.svg') }}"> lihat demo</a>
-            </div>
-            <div class="card" data-aos="zoom-in" data-aos-once="true">
-                <img src="{{ asset('Mockup/lg.png') }}">
-                <div class="price">Rp. 45.000</div>
-                <div class="title">Luxus-Gremy</div>
-                <a href="/wira-fitriani" target="_blank"><img src="{{ asset('home/eye.svg') }}"> lihat demo</a>
-            </div>
-            <div class="card" data-aos="zoom-in" data-aos-once="true">
-                <img src="{{ asset('Mockup/lp.png') }}">
-                <div class="price">Rp. 45.000</div>
-                <div class="title">Luxus-Pinky</div>
-                <a href="/farhan-rara" target="_blank"><img src="{{ asset('home/eye.svg') }}"> lihat demo</a>
             </div>
         </div>
     </div>
