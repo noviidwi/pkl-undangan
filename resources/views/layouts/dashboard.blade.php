9abaf6199209ff421e6d76be3f757b8944e284ee
pkl-undangan <novidwifitriani@students.undip.ac.id>
2024-02-06 11:10:49 +0700

    commit

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -26,6 +26,7 @@
                     <a href="/dashboard"><img src="{{asset('svg/other/home.svg')}}"> Home</a>
                     <a href="/dashboard/manage"><img src="{{asset('svg/other/manage.svg')}}">Manage Postingan</a>
                     <a href="/dashboard/create"><img src="{{asset('svg/other/plus.svg')}}">Tambah Postingan</a>
+                    <a href="/dashboard/comment"><img src="{{asset('svg/other/comment.svg')}}">Komentar</a>
                 </div>
                 <form action="/logout" method="POST" id="logout">
                     @csrf
