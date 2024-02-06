c542a6e5243c4baea38cb001ec541fd96a2f21e9
pkl-undangan <novidwifitriani@students.undip.ac.id>
2024-02-06 10:15:25 +0700

    com

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="{{asset('css/home.css') }}">
+    <title>Dashboard </title>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <link rel="stylesheet" href="{{asset('css/dashboard/dashboard.css')}}">
     <link rel="stylesheet" href="{{asset('css/font.css')}}">
     @yield('css')
@@ -17,7 +17,7 @@
                 <div class="logo-container">
                     <img src="{{asset('favicon/logo.png')}}" alt="Logo">
                     <div class="logo-text">
-                        <span>Cerita </span>
+                        <span>Cerita Bahagianya</span>
                     </div>
                 </div>
             </div>
@@ -61,7 +61,7 @@
         }
 
         function checkTime(i) {
-        if (i < 10) {i = "0" + i};
+        if (i < 10) {i = "0" + i}; 
         return i;
         }
         startTime()
