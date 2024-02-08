d86b3ac2410026bcd39a2a76a466e32aafe8de79
pkl-undangan <novidwifitriani@students.undip.ac.id>
2024-02-09 02:54:00 +0700

    com

--- resources/views/admin/transaksi/index.blade.php
+++ resources/views/admin/transaksi/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.dashboard')
+
 @section('css')
-    <link rel="stylesheet" href="{{asset('css/dashboard/manage.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard/manage.css') }}">
 @endsection
+
 @section('mainbar')
     <div class="search">
         <form action="/dashboard/manage" method="GET">
             <input type="text" name="search" placeholder="Masukkan slug">
-            <button type="submit"><img src="{{asset('svg/other/search.svg')}}"></button>
+            <button type="submit"><img src="{{ asset('svg/other/search.svg') }}"></button>
         </form>
     </div>
     @if (session('success'))
         <div class="alertSuccess">
-            {{session('success')}}
+            {{ session('success') }}
         </div>
     @endif
     <table cellspacing="0">
@@ -32,18 +34,18 @@
                     <td>{{ $data->tanggal_transaksi }}</td>
                     <td>{{ $data->formatRupiah('jumlah_transaksi') }}</td>
                     <td class="action">
-                        <a href="/dashboard/transaksi/{{$data->id_transaksi}}/edit" class="edit"><img src="{{asset('svg/other/edit.svg')}}"></a>
-                        <form action="/dashboard/transaksi/{{$data->id_transaksi}}" method="POST">
-                            @method('delete')
+                        <a href="/dashboard/transaksi/{{ $data->id_transaksi }}/edit" class="edit"><img src="{{ asset('svg/other/edit.svg') }}"></a>
+                        <form action="{{ route('destroyTransaksi', $data->id_transaksi) }}" method="POST">
                             @csrf
-                            <button onclick="return confirm('Delete?')"><img src="{{asset('svg/other/delete.svg')}}"></button>
+                            @method('DELETE')
+                            <button type="submit" onclick="return confirm('Apakah Anda yakin ingin menghapus transaksi ini?')">
+                                <img src="{{ asset('svg/other/delete.svg') }}">
+                            </button>
                         </form>
+                        
                     </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
-    @endsection
-
-
-    
+@endsection
